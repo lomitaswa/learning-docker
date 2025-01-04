3436fa03de57c2f441a57620cff6b285f344d2b1
lomitaswa_suna <lomitaswa.suna@ei.study>
2025-01-04 19:48:33 +0530

    modified start command for Docker.dev

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Text is changed from the feature branch
+          learn react .... Text is changed from the feature branch
         </a>
       </header>
     </div>
